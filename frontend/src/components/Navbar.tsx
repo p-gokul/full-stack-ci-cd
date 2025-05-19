5f17e9e592f96024f35fb0ab05976a13284ee4c5
Gokul Pant <77231879+p-gokul@users.noreply.github.com>
2025-05-19 22:42:47 +0900

    small update

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -4,7 +4,7 @@ import { ModeToggle } from "./ModeToggle";
 const Navbar: React.FC = () => {
   return (
     <div className="w-screen h-auto bg-red-30 flex flex-row justify-around items-center py-2">
-      <h3>Todo App = V2 =</h3>
+      <h3>Todo App = V3 =</h3>
       <div>
         <ModeToggle />
       </div>
